e7f5764fb8688f67cf29bcc6081401a7f72abbf4
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2022-12-09 18:56:46 -0500

    modified exercises 1,2, and 3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let len = string.length;
+    let list = [];
+    let output = "";
+    for (let i = 0; i < len;i++){
+        list.push(string.charAt(i));
+    }
+    for (let j = len-1; j>=0; j--){
+        output = output + list[j];
+    }
+    return output;
 
 };
-
+//solution on TOP return string.split('').reverse().join('');
+//.split('') turns a string into a list splitting at every specified character, if left blank it will split each character individually
+//now that its a list you can reverse() the list
+//.join('') returns a string that is an array concatenated. .join('') means join at each character
 // Do not edit below this line
 module.exports = reverseString;
