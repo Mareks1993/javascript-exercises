df59e6bb1f50111163d14ad31f28a7dfff16ef39
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2022-12-10 20:54:58 -0500

    added solution to leapYears and compared/listed differences between my solution and TOP's solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeapYear = false;
+    if (year%4 == 0 && year%100 != 0) isLeapYear = true;
+    else if (year%100 == 0 && year%400 == 0) isLeapYear = true;
+    return isLeapYear;
 
+    //TOP SOLUTION
+    //return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
+    //same as mine, they just combined both of my conditional statements into one and had the return be that output
 };
 
 // Do not edit below this line
