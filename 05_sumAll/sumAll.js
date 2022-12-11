2d2c93b42d3cc6445f8cbcd534d23be72185a187
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2022-12-10 20:43:23 -0500

    updated sumAll function utilizing Math.min and Math.max as well as a while loop to calculate the sum between the range starting at the min and ending at the max.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(args) {
+    if(Number.isInteger(arguments[0]) && arguments[0] > 0 && Number.isInteger(arguments[1]) &&  arguments[1] >0){
+        let count = Math.min(arguments[0],arguments[1]);
+        let sum = 0;
+        while (count <= Math.max(arguments[0],arguments[1])){
+            sum += count;
+            count++;
+        }
+        return sum;
+    }
+    else{
+        return "ERROR"
+    }
 
 };
 
