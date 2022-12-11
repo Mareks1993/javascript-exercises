5a56b1df20131e74888af8f23f1716b486b8d480
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2022-12-10 20:18:33 -0500

    updated removeFromArray.js with the solution. Also included comments on TOPs solution and compared.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, number) {
+    let tempList = list;
+    for (let i = 1; i<arguments.length; i++){
+        for (let j = tempList.length; j>=0;j--){
+            if (tempList[j] === arguments[i]){
+                tempList.splice(j,1);
 
+            }
+        }
+    //TOP solution is the following
+    //const newArray = [];
+    //tempList.forEach((item) => {
+    //if (!args.inclusdes(items)){newArray.push(item);}
+    //}
+    //this is one line shorter and basically says loop through each item in the original array, if the elemnt is not include in the arguments, push it to our newArray
+    }
+    return tempList;
 };
 
 // Do not edit below this line
