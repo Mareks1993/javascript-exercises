9994db0f8a3b42453711b83accc178cb98a8a0b6
Mareks1993 <marekstaszkiewicz@live.com>
2023-01-02 14:42:45 -0500

    modified function to have a for loop which pushed the next part of the fibonacci sequence to array fib. will output the specific index specified as a paramter.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) return "OOPS";
+    fib = [];
+    
+    for ( let i = 0; i < index;i++){
+        if(i < 2){
+            fib.push(1);
+        }
+        else{
+            fib.push(fib[i-1]+fib[i-2]);
+        }
+    }
+
+    return fib[index-1];
+
+
 
 };
 
