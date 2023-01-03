ed0cc94d363bc192227d85dba8156dd753f9ec88
Mareks1993 <marekstaszkiewicz@live.com>
2023-01-02 23:47:10 -0500

    created getAge function which will return the age of a person passed as a parameter with still alive conditionals. used reduce in FindTheOldest to loop through the array of person objects.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest,currentPerson) => 
+        {if (getAge(oldest)>getAge(currentPerson)){
+            return oldest;
+        }
+        else{
+            return currentPerson;
+        }
+    }
+    );
 };
 
+
+function getAge(person){
+    if(!("yearOfDeath" in person)){
+        return ((new Date().getFullYear()) - person.yearOfBirth);
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
